bf3c153a94e4dc62c812e822fda9b8763a1a8064
Sanjith S <sanjithsece@gmail.com>
2025-07-15 06:29:44 +0530

    login

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Login from "./components/Loginform";
+function App() {
+  return(
+    <div>
+      <Login />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Loginform.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import axios from "axios";
+
+const Loginform = () => {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleLogin(event) {
+    event.preventDefault();
+    try {
+      const res = await axios.post("https://springboot-ems-backend-1.onrender.com/api/auth/login", {
+        username: userName,
+        password: password,
+      });
+
+      console.log("Token:", res.data.token);
+      alert("Login Successful....");
+    
+    } catch (e) {
+      console.log("Login Error", e);
+      alert("Invalid Credentials");
+    }
+    console.log("Form Submitted");
+  }
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <div>
+        <form onSubmit={handleLogin}>
+          <label htmlFor="userName">User Name</label>
+          <input
+            id="userName"
+            name="userName"
+            value={userName}
+            type="text"
+            onChange={(e) => setUserName(e.target.value)}
+          />
+          <br /> <br />
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            name="password"
+            value={password}
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <br />
+          <br />
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Loginform;
